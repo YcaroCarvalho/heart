9b8e73dc17f9ffb4e9dfba928a12d3c396f74c88
felipr <100290286+felipehdev@users.noreply.github.com>
2022-10-04 15:25:17 -0300

    requisiçoes implementadas com sucesso

--- src/pages/ProtectedPage/ProtectedPage.jsx
+++ src/pages/ProtectedPage/ProtectedPage.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
+import api from "../../config/configApi";
 
 const ProtectedPage = () => {
-  const navigate = useNavigate();
+  const [data, setData] = useState([]);
   
+  const navigate = useNavigate();
+
+  //  useeffect requisiçao pra api
+
+  const getPosts = async () => {
+    await api
+      .get("/uploadImg")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  // useState que salva a requisiçao
   return (
     <div>
-      ProtectedPage
-      <button onClick={()=> { navigate("/"); localStorage.removeItem('user') }}>Sair</button>
+      <h1>colocar um texto aqui</h1>
+
+      <div>
+        {
+        data.map(post => (
+          <div key={post.imgId}>
+            <span>{post.src}</span> <br />
+            <span>{post.text}</span> <br />
+          </div>
+        ))}
+      </div>
       
+
+      <button
+        onClick={() => {
+          navigate("/");
+          localStorage.removeItem("user");
+        }}
+      >
+        Sair
+      </button>
     </div>
   );
 };
